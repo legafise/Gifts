f77c2b5a8f88cbe3105e88f7350df5aae3e18fbd
legafise <lashkevich1974@gamil.com>
2022-02-21 15:14:42 +0300

    Redesigned dao tests for spring boot

--- gifts-logic/src/main/java/com/epam/esm/config/LogicSpringConfig.java
+++ gifts-logic/src/main/java/com/epam/esm/config/LogicSpringConfig.java
@@ -4,13 +4,32 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class LogicSpringConfig {
+    private static final String DATA_BASE_STRUCTURE_SCRIPT = "sql/db_structure.sql";
+    private static final String DATA_BASE_DATA_SCRIPT = "sql/db_data.sql";
+    private static final String UTF8_ENCODING = "UTF-8";
+
     @Bean
+    @Profile("test")
+    public DataSource testDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding(UTF8_ENCODING)
+                .addScript(DATA_BASE_STRUCTURE_SCRIPT)
+                .addScript(DATA_BASE_DATA_SCRIPT)
+                .build();
+    }
+
+    @Bean
+    @Profile("prod")
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();

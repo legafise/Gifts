f57a7159fcd75af445477e6d725ba4816d055724
legafise <lashkevich1974@gamil.com>
2022-03-03 21:18:23 +0300

    Create user dao

---
+++ gifts-logic/src/main/java/com/epam/esm/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.epam.esm.dao;
+
+import com.epam.esm.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    Optional<User> findById(long id);
+    List<User> findAll();
+}
---
+++ gifts-logic/src/main/java/com/epam/esm/dao/impl/UserDaoImpl.java
@@ -0,0 +1,39 @@
+package com.epam.esm.dao.impl;
+
+import com.epam.esm.dao.UserDao;
+import com.epam.esm.entity.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+    private EntityManager entityManager;
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Optional<User> findById(long id) {
+        return Optional.ofNullable(entityManager.find(User.class, id));
+    }
+
+    @Override
+    public List<User> findAll() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> query = criteriaBuilder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        query.select(root);
+        TypedQuery<User> findAllQuery = entityManager.createQuery(query);
+        return findAllQuery.getResultList();
+    }
+}
